bc45ec86e1003220fe8a52b55b8caf8f39cef2d2
RIFAT ABRAR JOWAD <rajdiprifat2004@gmail.com>
2021-07-05 20:58:58 +0600

    click alart

--- hello-world/src/components/Home.vue
+++ hello-world/src/components/Home.vue
@@ -1,8 +1,20 @@
 <template>
-  <h1>Hello From Home component</h1>
+  <div>
+    <h1>Hello From Home component</h1>
+    <button @click="hello">Hello from home</button>
+  </div>
 </template>
 <script>
 export default {
   name: "Home",
+  props: {
+    data: String,
+    msg: String,
+  },
+  methods: {
+    hello() {
+      console.warn("alert from hello funtion");
+    },
+  },
 };
 </script>
